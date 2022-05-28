48f64d7117aeeac38eb482483ae5321e6316c367
VINAMRA-123 <100684691+VINAMRA-123@users.noreply.github.com>
2022-05-28 10:46:13 -0700

    Update game.py

--- game.py
+++ game.py
@@ -1,129 +1,4 @@
 
-
-# from cgitb import text
-# from email.mime import image
-# import math
-# from random import random
-# from tkinter import X, Y
-
-# import pygame
-# pygame.init()
-
-# backgnd = (0,0,255)
-# players = (0,255,255)
-
-# background = pygame.image.load('background.png')
-
-# def player():
-#     screen.blit((playerImg),(X,Y))
-
-# playerImg = pygame.image.load('player copy.png')
-# playerX = 370
-# playerY = 480
-# playerX_change = 0
-
-# bulletImg = pygame.image.load('bullet.png')
-# bulletX = 0
-# bulletY = 480
-# bulletX_change = 0
-# bulletY_change = 10
-# bullet_state = "ready"
-
-
-# def fire_bullet(x, y):
-#     global bullet_state
-#     bullet_state = "fire"
-#     screen.blit(bulletImg, (x + 16, y + 10))
-
-# def enemy():
-#     screen.blit((enemyImg),(X,Y))
-#     # for background 
-#     screen.blit(background,(0,0))
-    
-# def iscollision():
-#     distance = math.sqrt(math.pow(enemyX-bulletX,2)+math.pow(enemyY-bulletX_change,2))
-#     if distance<=27:
-#         return True
-#     else:
-#         return False
-    
-# enemyImg = pygame.image.load('enemy.png')
-# enemyX = random.randint(0,800)
-# enemyY = random.randint(50,150)
-# enemy_change = 0.3
-
-# screen = pygame.display.set_mode((1000, 700))
-# icon = pygame.image.load('ufo.png')
-# pygame.display.set_icon(icon)
-# pygame.display.set_caption("dx ball")
-
-# score_value = 0
-# FONT = pygame.font.Font('freesansbold.ttf',32 )
-
-# textX = 10
-# textY = 10
-# def show_case(x,y):
-#     score = FONT.render("SCORE :"+score_value,True,(0,0,255))
-#     screen.blit((score),(X,Y))
-
-# running = True
-# while running:
-#     screen.blit(0,0,255)
-#     playerX = None                       
-#     playerY = None
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT:
-#             running = False
-            
-#         if pygame.event.get() == pygame.KEYDOWN:
-#             if event.key == pygame.K_LEFT:
-#                 playerX_change =- 0.1
-            
-#             if event.key == pygame.K_RIGHT:
-#                 playerX_change =+ 0.1
-                
-#             if event.key == pygame.K_SPACE:
-#                 if bullet_state is "ready" :
-#                     fire_bullet(playerX,bulletY)
-                              
-#         if pygame.event.get() == pygame.KEYUP:
-#             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-#                 playerX_change =0
-
-#     if playerX<=0:
-#         playerX = 0
-#     if playerX>= 736:
-#         playerX = 736
-        
-#     if enemyX<0:
-#         enemyX += enemy_change
-#     if enemyX>736:
-#         enemyX -= enemy_change
-        
-#     collision = iscollision(enemyX,enemyY,bulletX,bulletY)
-#     if collision:
-#         bulletY = 480
-#         bullet_state = "ready"
-#         score_value += 1
-#         enemyX = random.randint(0,800)
-#         enemyY = random.randint(50,150)
-#         enemy(enemyX,enemyY)
-        
-#     if bulletY<=0:
-#         bulletY = 480
-#         bullet_state = "ready"
-        
-#     if bullet_state is "fire":
-#         fire_bullet(playerX,bulletY)
-#         bulletY-= bulletY_change
-              
-#     playerX += playerX_change        
-#     player(playerX,playerY)
-#     show_case(textX,textY)
-    
-#     pygame.display.update()
-    
-    # **************************************************************
     
 import math
 import random
@@ -135,7 +10,7 @@ from pygame import mixer
 pygame.init()
 
 # create the screen
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((800,650))
 
 # Background
 background = pygame.image.load('background.png')
@@ -161,7 +36,7 @@ enemyX = []
 enemyY = []
 enemyX_change = []
 enemyY_change = []
-num_of_enemies = 6
+num_of_enemies = 7
 
 for i in range(num_of_enemies):
     enemyImg.append(pygame.image.load('enemy.png'))
@@ -215,7 +90,7 @@ def enemy(x, y, i):
 def fire_bullet(x, y):
     global bullet_state
     bullet_state = "fire"
-    screen.blit(bulletImg, (x + 16, y + 10))
+    screen.blit(bulletImg, (x + 15, y + 10))
 
 
 def isCollision(enemyX, enemyY, bulletX, bulletY):
